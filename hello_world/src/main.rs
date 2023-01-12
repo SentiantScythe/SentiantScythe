ffec70a6aaed583146287e9cf5fb8ec699e4c9b9
Danniel Tapfield <danniel.tapfield@gmail.com>
2023-01-13 01:30:06 +1300

    Update 13th

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -5,11 +5,13 @@ use std::io::{Write,BufReader,BufRead,ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
 
+
 fn main() {
     print!("What is your name?");
     let mut name: String = String::new();
     let greeting: &str = "nice to meet you";
-    io::stdin().read_line(Buf: &mut name) Result <usize, Error>
-    .expect(msg: "Didnt recieve input");
+    io::stdin().read_line(Buf: &mut name) Result<usize,
+     Error>
+        .expect(msg: "Didnt recieve input");
     println!("Hello{}!!! {}", name.trim_end(), greeting);
 }
