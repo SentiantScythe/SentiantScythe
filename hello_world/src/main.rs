a4c2aae16bcc1169933177de868d0d4eb8e61fd3
Danniel Tapfield <danniel.tapfield@gmail.com>
2023-01-18 01:35:25 +1300

    DT

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,16 +1,4 @@
-#![allow(unused_imports)]
-
-use std::io::{self, Read};
-use rand::Rng;
-use std::io::{Write,BufReader,BufRead,ErrorKind};
-use std::fs::File;
-use std::cmp::Ordering;
-
 fn main()
 {
-    print!("What is your name?");
-    let mut name: String = String::new();
-    let greeting: &str = "nice to meet you";
-    io::stdin().read_line(BufRead: &mut name) Result<usize,Error>.expect(msg: "Didnt recieve input");
-    println!("Hello{}!!! {}", name.trim_end(), greeting);
+    println!("Hello, World")
 }
