bbc72528d1c8f3e0d0045cdc46ad82c4c20b4bfa
Danniel Tapfield <thefourthcoming91@gmail.com>
2025-07-17 14:15:14 +1200

    starting

---
+++ fedbox/src/index.ts
@@ -0,0 +1,9 @@
+// This file serves as the entry point of the application. 
+// It contains the main logic for the workspace, including initialization code and function definitions.
+
+function main() {
+    console.log("Welcome to the Fedbox!!");
+    // Additional initialization code can go here
+}
+
+main();
